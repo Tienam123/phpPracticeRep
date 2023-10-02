6084f4a30628a2d0ca220b282ebf5cb165cbb183
Vladyslav <dr.tienam123@gmail.com>
2023-10-02 11:39:30 +0300

    complete task 319

--- src/js/practice-template.js
+++ src/js/practice-template.js
@@ -1,32 +1,5 @@
-console.log('Hello world');
-console.log(new Date);
-
-function addZero(num) {
-  if (num >= 0 && num <= 9) {
-    return '0' + num;
-  } else {
-    return num;
-  }
-}
-
-let date = new Date();
-
-console.log(addZero(date.getFullYear()) + '-' + addZero(date.getMonth() + 1) + '-' + addZero(date.getDate()));
-
-console.log(addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' + addZero(date.getDate()) + '.' + addZero(date.getMonth() + 1) + '.' +
-                addZero(date.getFullYear()));
-
-//Сколько дней осталось до близжайшего восвресенья
-const today = new Date(2023, 9, 8).getTime();
-console.log('timestamp Sunday:', today);
-const newDay = new Date().getTime();
-const quantityDays = (today - newDay) / 1000 / 60 / 60 / 24;
-console.log('До близжайшего воскресенья осталось :', Math.round(quantityDays));
-
-// 2 вариант
-if (date.getDay() !== 0) {
-  let a = 7;
-  let quantityDaysToSunday = a - date.getDay();
-  console.log(quantityDaysToSunday);
-}
-
+let now = new Date(2010, 1, 15);
+let date = new Date(2000, 8, 1);
+console.log('Between this dates is :', Math.round((now - date) / (1000 * 60 * 60 * 24)), ' days');
+console.log('Between this dates is :', Math.round((now - date) / (1000 * 60 * 60 * 24 * 365 / 12)), ' month');
+console.log('Between this dates is :', Math.round((now - date) / (1000 * 60 * 60 * 24 * 365)), ' years');
