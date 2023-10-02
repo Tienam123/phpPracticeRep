511e6a08686c17a87a2cce98a1427289fa444557
Vladyslav <dr.tienam123@gmail.com>
2023-10-02 12:57:16 +0300

    complete task 330 JS code.mu

--- src/js/practice-template.js
+++ src/js/practice-template.js
@@ -1,10 +1,14 @@
 const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота'];
 
-function getDayPreviousMonth(month, day = 1) {
-  month -= 1;
-  let now = new Date();
-  let result = new Date(now.getFullYear() - 1, month, day);
-  return days[result.getDay()];
+let now = new Date();
+let finishDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24, 0, 0);
+console.log((now - finishDay) / (1000 * 60 * 60));
+
+function getCurrentTime(time) {
+  const hours = Math.trunc(time);
+  const minutes = Math.trunc(60 * (time % 1).toFixed(2));
+
+  console.log('Прошло ', hours, ' часов и ', minutes, ' минут');
 }
 
-console.log(getDayPreviousMonth(10, 2));
+getCurrentTime((finishDay - now) / (1000 * 60 * 60));
