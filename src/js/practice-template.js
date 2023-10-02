497ae75baed635a35a649b761c65c8232cd0b109
Vladyslav <dr.tienam123@gmail.com>
2023-10-02 12:03:45 +0300

    Added function isCorectDate

--- src/js/practice-template.js
+++ src/js/practice-template.js
@@ -1,21 +1,11 @@
-function getQuantityDaysOFMoth(year, month = 0) {
-  return new Date(year, month, 0);
-}
-
 const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота'];
 
-console.log(getQuantityDaysOFMoth(4, 2025), days[getQuantityDaysOFMoth(4, 2025).getDay()]);
-console.log(getQuantityDaysOFMoth(1990));
-
-function isLeapYear(year, month = 2, day = 0) {
+function isCorrectDate(day, month, year) {
   const date = new Date(year, month, day);
-  if (date.getDate() === 29) {
-    console.log('Год высокосный');
-  } else {
-    console.log('Не высокосный');
+  if (date.getDate() == day && date.getMonth() == month && date.getFullYear() == year) {
+    return 'Is corrent Date';
   }
-
+  return 'Incorrect Date';
 }
 
-isLeapYear(1996);
-
+console.log(isCorrectDate(0, 10, 1990));
