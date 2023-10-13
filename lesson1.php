c509cdc4b0d3ba9df82cbda764cb0415e8f29d3c
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-13 16:06:58 +0300

    types of variables

---
+++ lesson1.php
@@ -0,0 +1,10 @@
+<?php
+$a = array(1,'23',false);
+var_dump($a);
+
+$b = ['wallet'=> 500,'convert'=>'course project','body'=>false];
+var_dump($b);
+$obj = new stdClass();
+var_dump($obj);
+$obj-> a = 'I am property';
+var_dump($obj);
