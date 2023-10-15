a42d3d0e1c8ca276825571a613284aeac2a34230
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-15 06:26:33 +0300

    task # 308

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <!doctype html>
-<html lang="en">
+<html lang='en'>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Document</title>
-    <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
-<header></header>---
-<main>
-    <div class="main__container">
-        <h1>Hello world</h1>
-    </div>
-</main>
-<footer></footer>
-<script src="./index.js"></script>
-<?php ?>
+<?php
+unlink( 'index.js' );
+
+?>
 </body>
 </html>
