37a0830f92fece404e9463d7e0a3d6ba79b9dab6
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-22 07:20:14 +0300

    Практика по SELECT из баз данных на страницу html

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
+$host     = 'localhost';
+$user     = 'root';
+$password = '';
+$name     = 'code_mu';
 
-$_GET['dkfljsfksdjf'] = 'Uzhas';
 
-function &reMake( $str ) {
-	$_GET['dkfljsfksdjf'] = $str;
+$connect = mysqli_connect( $host, $user, $password, $name );
+mysqli_query( $connect, "SET NAMES 'utf8'" );
+$query = "SELECT * FROM users";
+$res = mysqli_query( $connect, $query ) or die( mysqli_error( $connect ) );
 
-	return $_GET['dkfljsfksdjf'];
+for ( $data = []; $result = mysqli_fetch_assoc( $res ); $data[] = $result ) {
 }
+var_dump( $data );
 
-$new = &reMake( 'Ne uszas' );
-var_dump( $new );
-
-$new = '2222222';
-
-var_dump( $new );
-var_dump( $_GET['dkfljsfksdjf'] );
+?>
+</body>
+</html>
--- users.sql
+++ users.sql
@@ -1,16 +1,70 @@
-create table users
-(
-    id     int auto_increment
-        primary key,
-    name   varchar(24) null,
-    age    varchar(24) null,
-    salary int         null
-)
-    collate = utf8mb4_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 22 2023 г., 06:16
+-- Версия сервера: 10.8.4-MariaDB-log
+-- Версия PHP: 8.1.9
 
-INSERT INTO my_db.users (name, age, salary) VALUES ('user1', '23', 400);
-INSERT INTO my_db.users (name, age, salary) VALUES ('user2', '25', 500);
-INSERT INTO my_db.users (name, age, salary) VALUES ('user3', '23', 500);
-INSERT INTO my_db.users (name, age, salary) VALUES ('user4', '30', 900);
-INSERT INTO my_db.users (name, age, salary) VALUES ('user5', '27', 500);
-INSERT INTO my_db.users (name, age, salary) VALUES ('user6', '28', 900);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `code_mu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) NOT NULL,
+  `salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `age`, `salary`) VALUES
+(1, 'user1', 23, 400),
+(2, 'user2', 24, 500),
+(3, 'user3', 26, 500),
+(4, 'user4', 33, 700);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
